8beaf60fc1597a11596548e76e6bfae95677e83a
Sucala Paul <sucala.paul@gmail.com>
2013-10-07 15:53:43 +0300

    Diblook Setup

---
+++ resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by diblook.rc
+//
+#define IDR_MAINFRAME                   2
+#define IDR_DIBTYPE                     3
+#define IDS_DIB_TOO_BIG                 4
+#define IDS_CANNOT_LOAD_DIB             5
+#define IDS_CANNOT_SAVE_DIB             6
+#define IDD_ABOUTBOX                    100
+#define ID_PROCESSING_DUPLICATE         32770
+#define ID_PROCESSING_PARCURGERESIMPLA  32771
+#define ID_PROCESSING_MULTIPLETHRESHOLDING 32772
+#define ID_PROCESSING_L1                32773
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         102
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
